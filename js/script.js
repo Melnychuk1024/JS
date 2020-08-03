bc20ac94e42c7600f42bf041f14b3d5fcc4d423d
Dima <example@gmail.com>
2020-08-03 15:42:13 +0300

    Lesson 20

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-'use strict';
+"use strict";
 
-const numbersOfFilms = prompt('How much films you watching?','');
-
-const lastFilmOne = prompt('Останній фільм'),
-    OcinkaFilmOne = prompt('Оцінка'),    
-    lastfilmTwo = prompt('Останній фільм'),
-    OcinkaFilmTwo = prompt('Оцінка');
-
-const personalMovieDB = {
-    count: numbersOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const obj = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
 };
 
-personalMovieDB.movies[lastFilmOne] = OcinkaFilmOne;
-personalMovieDB.movies[lastfilmTwo] = OcinkaFilmTwo;
+// obj.makeTest();
 
-console.log(personalMovieDB);
+// const {border, bg} = obj.colors;
+// console.log(border, bg);
+
+// delete obj.name;
+// console.log(obj);
+
+// for (let key in obj) {
+//     if (typeof(obj[key]) === 'object') {
+//         for (let i in obj[key]) {
+//             console.log(`Ключ ${i} має значення ${obj[key][i]}`);        
+//         }
+//     } else {
+//         console.log(`Ключ ${key} має значення ${obj[key]}`);
+//     }
+// }
+
+// console.log(Object.keys(obj).length);
+
